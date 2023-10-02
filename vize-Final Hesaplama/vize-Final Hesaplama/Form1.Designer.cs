3539a88739c44945f5859b39beeadb4b0e8c2d49
DilanKaya <145452308+DilanKaya2013@users.noreply.github.com>
2023-10-02 16:09:32 +0300

    Add files via upload

---
+++ vize-Final Hesaplama/vize-Final Hesaplama/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace vize_Final_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVize = new System.Windows.Forms.Label();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.txtVize = new System.Windows.Forms.TextBox();
+            this.txtFinal = new System.Windows.Forms.TextBox();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblVize
+            // 
+            this.lblVize.AutoSize = true;
+            this.lblVize.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblVize.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblVize.Location = new System.Drawing.Point(34, 64);
+            this.lblVize.Name = "lblVize";
+            this.lblVize.Size = new System.Drawing.Size(41, 16);
+            this.lblVize.TabIndex = 0;
+            this.lblVize.Text = "Vize:";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblFinal.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblFinal.Location = new System.Drawing.Point(29, 113);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(45, 16);
+            this.lblFinal.TabIndex = 1;
+            this.lblFinal.Text = "Final:";
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrtalama.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.lblOrtalama.Location = new System.Drawing.Point(29, 248);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(74, 16);
+            this.lblOrtalama.TabIndex = 2;
+            this.lblOrtalama.Text = "Ortalama:";
+            // 
+            // txtVize
+            // 
+            this.txtVize.Location = new System.Drawing.Point(101, 63);
+            this.txtVize.Name = "txtVize";
+            this.txtVize.Size = new System.Drawing.Size(100, 20);
+            this.txtVize.TabIndex = 3;
+            // 
+            // txtFinal
+            // 
+            this.txtFinal.Location = new System.Drawing.Point(101, 113);
+            this.txtFinal.Name = "txtFinal";
+            this.txtFinal.Size = new System.Drawing.Size(100, 20);
+            this.txtFinal.TabIndex = 4;
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.SystemColors.ControlText;
+            this.btnHesapla.ForeColor = System.Drawing.Color.Crimson;
+            this.btnHesapla.Location = new System.Drawing.Point(101, 169);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(100, 43);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Crimson;
+            this.ClientSize = new System.Drawing.Size(275, 316);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtFinal);
+            this.Controls.Add(this.txtVize);
+            this.Controls.Add(this.lblOrtalama);
+            this.Controls.Add(this.lblFinal);
+            this.Controls.Add(this.lblVize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVize;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.TextBox txtVize;
+        private System.Windows.Forms.TextBox txtFinal;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
